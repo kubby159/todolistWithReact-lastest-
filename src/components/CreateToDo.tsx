be3bed1ce99cfec8132614af2189f7a45ef527b2
kubby159 <83577397+kubby159@users.noreply.github.com>
2022-03-11 19:33:02 +0900

    [React] style done!

--- src/components/CreateToDo.tsx
+++ src/components/CreateToDo.tsx
@@ -2,6 +2,8 @@ import { useForm } from "react-hook-form";
 import { useSetRecoilState } from "recoil";
 import { toDoState } from "./atoms";
 import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 const ToDoForm = styled.form`
   padding: 1rem;
@@ -59,20 +61,7 @@ function CreateToDo() {
         placeholder="Write a to do"
       />
       <ToDoAddBtn>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          strokeWidth={2}
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M12 4v16m8-8H4"
-          />
-        </svg>
+        <FontAwesomeIcon icon={faPlus} size="lg"></FontAwesomeIcon>
       </ToDoAddBtn>
     </ToDoForm>
   );
